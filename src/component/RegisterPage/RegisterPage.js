4cbd035103778b536a850292f0d6ae86e4365ca5
ThinhNgo767 <thinhngo767@gmail.com>
2025-08-08 14:23:05 +0700

    fix

--- src/component/RegisterPage/RegisterPage.js
+++ src/component/RegisterPage/RegisterPage.js
@@ -2,7 +2,7 @@ import "./RegisterPage.css";
 import authAPI from "../../apis/authAPI";
 
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 import Swal from "sweetalert2";
